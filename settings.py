c9b0a750f74f831e1bc0402147efa1bacbbeebc1
Sofimpress <146110214+Sofimpress@users.noreply.github.com>
2024-05-23 18:38:59 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,34 @@
+BLACK = (0, 0, 0)
+FPS = 60
+SC_WIDTH = 800
+SC_HEIGHT = 600
+WHITE = (100, 100 ,100)
+
+PLAYER_FILE_NAME = "images\\player.png"
+PLAYER_FILE_NAME2 = "images\\player2.png"
+PLAYER_SPEED = 10
+
+PLAYER_MAX_HP = 100
+
+LASER_FILE_NAME = "images\\laser.png"
+LASER_SPEEDY = 40
+
+GAME_OVER_FILENAME = "images\\game_over.png"
+BACK_FILENAME = "images\\back.jpg"
+
+FIRE_DELAY = 1000
+
+BONUS_SPEED = 1
+BONUC_CHANCE = 100
+PILL_HP = 25
+
+SHIELD_FILENAME = "images\\shield.png"
+SHIELD_RUNTIME = 10000
+SHIELD_INACTIVE_POS = (100, 1000)
+
+THREE_LASER_RUNTIME = 15000
+
+EXPLOSION_FILENAME_PREFIX = "images\\regularExplosion0"
+EXPLOSION_FILENAME_POSTFIX = ".png"
+SPEED = 2
+
